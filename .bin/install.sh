0633090eea3803d9374c01853146aad901245123
ritosu <aunosumaho@yahoo.co.jp>
2022-07-30 17:25:00 +0900

    mod

--- .bin/install.sh
+++ .bin/install.sh
@@ -1,19 +1,14 @@
 #!/usr/bin/env bash
 set -ue
 
-helpmsg() {
-    command echo "Usage: $0 [--help | h ]" 0>&2
-    command echo ""
-}
-
 install_packages() {
-	command echo "install packages..."
+	echo "install packages..."
     os=$(whichOS)
 
 	if [ $os == "Darwin" ]; then
-	    command brew install zsh neovim tmux curl
+	    brew install zsh neovim tmux curl
     elif [ $os == "ubuntu" ]; then
-	    command apt-get install -y zsh neovim tmux curl
+        sudo apt update && sudo apt -y upgrade && sudo apt install -y zsh neovim tmux curl
 	else
 	    return 0;
 	fi
@@ -22,27 +17,23 @@ install_packages() {
 }
 
 link_to_homedir() {
-    command echo "backup old dotfiles..."
     if [ ! -d "$HOME/.dotbackup" ];then
-        command echo "$HOME/.dotbackup not found. Auto Make it"
-        command mkdir "$HOME/.dotbackup"
+        mkdir "$HOME/.dotbackup"
     fi
 
-    local script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
-    local dotdir=$(dirname ${script_dir})
+    script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+    dotdir=$(dirname ${script_dir})
     if [[ "$HOME" != "$dotdir" ]];then
         for f in $dotdir/.??*;do
             [[ `basename $f` == ".git" ]] && continue
             if [[ -L "$HOME/`basename $f`" ]];then
-                command rm -f "$HOME/`basename $f`"
+                rm -f "$HOME/`basename $f`"
             fi
             if [[ -e "$HOME/`basename $f`" ]];then
-                command mv "$HOME/`basename $f`" "$HOME/.dotbackup"
+                mv "$HOME/`basename $f`" "$HOME/.dotbackup"
             fi
-            command ln -snf $f HOME
+            ln -snf $f HOME
         done
-    else
-        command echo "same install src dest"
     fi
 }
 
@@ -56,8 +47,6 @@ whichOS() {
             echo ubuntu
             return
         fi
-    else
-        command echo "this os is not supported."
     fi
 }
 
