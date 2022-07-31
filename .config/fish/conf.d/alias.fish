c023b1b13898200df974d2ee9ffb1742d5309a68
ritosu <aunosumaho@yahoo.co.jp>
2022-07-31 23:23:31 +0900

    add fish file

---
+++ .config/fish/conf.d/alias.fish
@@ -0,0 +1,10 @@
+alias ls="ls -a"
+alias ll="ls -ahl"
+
+alias ga="git add"
+alias gb="git branch"
+alias gc="git commit"
+alias gco="git checkout"
+alias gfm="git pull"
+alias gp="git push"
+alias gs="git status"
