d7bbc5d0ef60c825dd45f7a5ff9e10acae02c7e0
ritosu <aunosumaho@yahoo.co.jp>
2022-08-10 20:31:18 +0900

    mod gitignore

---
+++ .config/fish/conf.d/mythema.fish
@@ -0,0 +1,12 @@
+# 色一覧
+set -l blue 005fff
+set -l sky_blue 00ffff
+set -l green 00ff3b
+set -l purple ff00ff
+set -l yellow ffff00
+set -l orange ff4f02
+set -l pink ff0461
+set -l white ffffff
+set -l black 000000
+
+
