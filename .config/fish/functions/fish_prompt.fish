d8a68b8d61a604111cc018c129aa7d05e9db85e8
ritosu <aunosumaho@yahoo.co.jp>
2022-12-28 17:40:16 +0000

    change color of fish prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,5 +1,5 @@
 function fish_prompt --description 'Write out the prompt'
-    echo -n -s (set_color yellow) "$USER" ' ' 
-    echo -n -s (set_color ff3300) (prompt_pwd) (set_color normal) 
+    echo -n -s (set_color -b blue) (set_color black) "$USER" ' ' 
+    echo -n -s (set_color -b brblue) (set_color black) (prompt_pwd) ' ' (set_color normal) 
     echo -n -s (fish_vcs_prompt) $normal " "$prompt_status $suffix " "
 end
