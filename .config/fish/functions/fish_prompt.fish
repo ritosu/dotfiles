df8fae30fc7a8ccaf6dff7ccb54582fc86c7dce1
tanaka kenta <kenta-tanaka@J0016.local>
2023-04-18 22:53:57 +0900

    不要なもの削除

--- .config/fish/functions/fish_prompt.fish
+++
@@ -1,5 +0,0 @@
-function fish_prompt --description 'Write out the prompt'
-    echo -n -s (set_color -b blue) (set_color black) "$USER" ' ' 
-    echo -n -s (set_color -b brblue) (set_color black) (prompt_pwd) ' ' (set_color normal) 
-    echo -n -s (fish_vcs_prompt) $normal " "$prompt_status $suffix " "
-end
